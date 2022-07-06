3aa229709478fe02783343df55c0c0c6d17ea84e
kavyasuribabu <kavyasuribabu9311@gmail.com>
2022-07-06 18:56:50 +0530

    firstcommit

---
+++ src/main/java/com/example/Addition.java
@@ -0,0 +1,23 @@
+
+package com.example;
+import java.util.Scanner;
+
+public class Addition {
+
+
+public static void main(String[] args) {
+ int a,b,c;
+ Scanner scan=new Scanner(System.in);
+ System.out.println("enter the a value");
+ a=scan.nextInt();
+ System.out.println("enter the b value");
+ b=scan.nextInt();
+ c=a+b;
+ System.out.println(c);
+
+}
+	
+
+
+	
+}
